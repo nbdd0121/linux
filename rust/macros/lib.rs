fff00461b0be7fd3ec218dcc428f25886b5ec04a
Gary Guo <gary@garyguo.net>
2023-03-21 15:08:41 +0000

    rust: macros: add `paste!` proc macro
    This macro provides a flexible way to concatenated identifiers together
    and it allows the resulting identifier to be used to declare new items,
    which `concat_idents!` does not allow. It also allows identifiers to be
    transformed before concatenated.
    
    The `concat_idents!` example
    
        let x_1 = 42;
        let x_2 = concat_idents!(x, _1);
        assert!(x_1 == x_2);
    
    can be written with `paste!` macro like this:
    
        let x_1 = 42;
        let x_2 = paste!([<x _1>]);
        assert!(x_1 == x_2);
    
    However `paste!` macro is more flexible because it can be used to create
    a new variable:
    
        let x_1 = 42;
        paste!(let [<x _2>] = [<x _1>];);
        assert!(x_1 == x_2);
    
    While this is not possible with `concat_idents!`.
    
    This macro is similar to the `paste!` crate [1], but this is a fresh
    implementation to avoid vendoring large amount of code directly. Also, I
    have augmented it to provide a way to specify span of the resulting
    token, allowing precise control.
    
    For example, this code is broken because the variable is declared inside
    the macro, so Rust macro hygiene rules prevents access from the outside:
    
        macro_rules! m {
            ($id: ident) => {
                // The resulting token has hygiene of the macro.
                paste!(let [<$id>] = 1;)
            }
        }
    
        m!(a);
        let _ = a;
    
    In this versionn of `paste!` macro I added a `span` modifier to allow
    this:
    
        macro_rules! m {
            ($id: ident) => {
                // The resulting token has hygiene of `$id`.
                paste!(let [<$id:span>] = 1;)
            }
        }
    
        m!(a);
        let _ = a;
    
    Link: http://docs.rs/paste/ [1]
    Signed-off-by: Gary Guo <gary@garyguo.net>

--- rust/macros/lib.rs
+++ rust/macros/lib.rs
@@ -1,10 +1,12 @@
 // SPDX-License-Identifier: GPL-2.0
 
 //! Crate for all kernel procedural macros.
+#![feature(let_else)]
 
 mod concat_idents;
 mod helpers;
 mod module;
+mod paste;
 mod vtable;
 
 use proc_macro::TokenStream;
@@ -165,4 +167,100 @@ pub fn vtable(attr: TokenStream, ts: TokenStream) -> TokenStream {
 #[proc_macro]
 pub fn concat_idents(ts: TokenStream) -> TokenStream {
     concat_idents::concat_idents(ts)
+}
+
+/// Paste identifiers together.
+///
+/// Within the `paste!` macro, identifiers inside `[<` and `>]` are concatenated together to form a
+/// single identifier.
+///
+/// This is similar to the [`paste`] crate, but with pasting feature limited to identifiers
+/// (literals, lifetimes and documentation strings are not supported). There is a difference in
+/// supported modifiers as well.
+///
+/// # Example
+///
+/// ```ignore
+/// use kernel::macro::paste;
+///
+/// macro_rules! pub_no_prefix {
+///     ($prefix:ident, $($newname:ident),+) => {
+///         paste! {
+///             $(pub(crate) const $newname: u32 = [<$prefix $newname>];)+
+///         }
+///     };
+/// }
+///
+/// pub_no_prefix!(
+///     binder_driver_return_protocol_,
+///     BR_OK,
+///     BR_ERROR,
+///     BR_TRANSACTION,
+///     BR_REPLY,
+///     BR_DEAD_REPLY,
+///     BR_TRANSACTION_COMPLETE,
+///     BR_INCREFS,
+///     BR_ACQUIRE,
+///     BR_RELEASE,
+///     BR_DECREFS,
+///     BR_NOOP,
+///     BR_SPAWN_LOOPER,
+///     BR_DEAD_BINDER,
+///     BR_CLEAR_DEATH_NOTIFICATION_DONE,
+///     BR_FAILED_REPLY
+/// );
+///
+/// assert_eq!(BR_OK, binder_driver_return_protocol_BR_OK);
+/// ```
+///
+/// # Modifiers
+///
+/// For each identifier, it is possible to attach one or multiple modifiers to
+/// it.
+///
+/// Currently supported modifiers are:
+/// * `span`: change the span of concatenated identifier to the span of the specified token. By
+/// default the span of the `[< >]` group is used.
+/// * `lower`: change the identifier to lower case.
+/// * `upper`: change the identifier to upper case.
+///
+/// ```ignore
+/// use kernel::macro::paste;
+///
+/// macro_rules! pub_no_prefix {
+///     ($prefix:ident, $($newname:ident),+) => {
+///         kernel::macros::paste! {
+///             $(pub(crate) const fn [<$newname:lower:span>]: u32 = [<$prefix $newname:span>];)+
+///         }
+///     };
+/// }
+///
+/// pub_no_prefix!(
+///     binder_driver_return_protocol_,
+///     BR_OK,
+///     BR_ERROR,
+///     BR_TRANSACTION,
+///     BR_REPLY,
+///     BR_DEAD_REPLY,
+///     BR_TRANSACTION_COMPLETE,
+///     BR_INCREFS,
+///     BR_ACQUIRE,
+///     BR_RELEASE,
+///     BR_DECREFS,
+///     BR_NOOP,
+///     BR_SPAWN_LOOPER,
+///     BR_DEAD_BINDER,
+///     BR_CLEAR_DEATH_NOTIFICATION_DONE,
+///     BR_FAILED_REPLY
+/// );
+///
+/// assert_eq!(br_ok(), binder_driver_return_protocol_BR_OK);
+/// ```
+///
+/// [`paste`]: https://docs.rs/paste/
+#[proc_macro]
+pub fn paste(input: TokenStream) -> TokenStream {
+    let mut tokens = input.into_iter().collect();
+    paste::expand(&mut tokens);
+    tokens.into_iter().collect()
 }
