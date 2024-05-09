84732eea159237f6b8e3575c64f91d57b4d2b8c9
Andreas Hindborg <a.hindborg@samsung.com>
2024-05-09 17:02:20 +0200

    rnull: add module drop implementation

--- drivers/block/rnull.rs
+++ drivers/block/rnull.rs
@@ -95,6 +95,7 @@ impl TryFrom<u8> for IRQMode {
     }
 }
 
+#[pin_data(PinnedDrop)]
 struct NullBlkModule {
     _disk: Pin<Box<Mutex<GenDisk<NullBlkDevice>>>>,
 }
@@ -128,6 +129,13 @@ impl kernel::Module for NullBlkModule {
         let disk = Box::pin_init(new_mutex!(disk, "nullb:disk"), flags::GFP_KERNEL)?;
 
         Ok(Self { _disk: disk })
+    }
+}
+
+#[pinned_drop]
+impl PinnedDrop for NullBlkModule {
+    fn drop(self: Pin<&mut Self>) {
+        pr_info!("Dropping rnullb\n");
     }
 }
 
