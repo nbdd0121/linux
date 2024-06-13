f80ca932c54175af6ce52f566c833e36bf73ae21
Andreas Hindborg <a.hindborg@samsung.com>
2024-06-13 11:29:42 +0200

    rust: block: add remote completion to `Request`

--- rust/kernel/block/mq/request.rs
+++ rust/kernel/block/mq/request.rs
@@ -160,6 +160,23 @@ impl<T: Operations> Request<T> {
         }
     }
 
+    /// Complete the request by scheduling `Operations::complete` for
+    /// execution.
+    ///
+    /// The function may be scheduled locally, via SoftIRQ or remotely via IPMI.
+    /// See `blk_mq_complete_request_remote` in [`blk-mq.c`] for details.
+    ///
+    /// [`blk-mq.c`]: srctree/block/blk-mq.c
+    pub fn complete(this: ARef<Self>) {
+        let ptr = this.into_raw().cast::<bindings::request>();
+        // SAFETY: By type invariant, `self.0` is a valid `struct request`
+        if !unsafe { bindings::blk_mq_complete_request_remote(ptr) } {
+            // SAFETY: We released a refcount above that we can reclaim here.
+            let this = unsafe { Request::aref_from_raw(ptr) };
+             T::complete(this);
+        }
+    }
+
     /// Get the target sector for the request
     #[inline(always)]
     pub fn sector(&self) -> usize {
