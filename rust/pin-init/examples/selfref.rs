bb70cec802fe397dd5bce5033315914c53469fae
Gary Guo <gary@garyguo.net>
2026-05-06 18:12:59 +0100

    rust: pin-init: internal: pin_data: support mutable borrows
    Allow fields to be mutably referenced by other fields in addition to shared
    references. In order for this to be sound, the fields that can be mutably
    borrowed are blocked from being accessed via field access syntax or
    projection to maintain the aliasing requirements.
    
    Signed-off-by: Gary Guo <gary@garyguo.net>

--- rust/pin-init/examples/selfref.rs
+++ rust/pin-init/examples/selfref.rs
@@ -6,12 +6,18 @@ use pin_init::*;
 struct SelfRef {
     part: &'str str,
     str: String,
+
+    #[borrows(mut mut_str)]
+    mut_part: &'mut_str mut str,
+    mut_str: String,
 }
 
 fn use_self_ref() {
     stack_pin_init!(let foo = pin_init!(SelfRef {
         str: "hello world".to_owned(),
         part: &str[..5],
+        mut_str: "hello world".to_owned(),
+        mut_part: &mut mut_str[..5],
     }));
 
     // Access via projection.
@@ -26,6 +32,13 @@ fn use_self_ref() {
     });
 
     println!("{}", foo.part());
+
+    // Access fields that mutable borrow others are similar to those of shared borrow.
+    println!("{}", foo.as_mut().project().mut_part);
+    println!("{}", foo.mut_part());
+    foo.as_mut().with_project(|proj| {
+        proj.mut_part.make_ascii_uppercase();
+    });
 }
 
 fn main() {
